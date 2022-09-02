74c36a6e33c8e35179ef12ccffc945af229ddb20
MelihCelik00 <melihsafa.c@gmail.com>
2022-09-02 15:46:32 +0300

    fixed second login issue

--- components/auth/controller.js
+++ components/auth/controller.js
@@ -48,18 +48,19 @@ const signup = async (req, res) => {
 
 const login = async (req, res) => {
     try {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
 
         // find a user by their email
-        const user = await User.findOne({email});
+        const user = await User.findOne({
+            where: {email: email}
+        });
 
         // if user email is found, compare password with bycrypt
         if (user) {
             const isSame = await bcrypt.compare(password, user.password);
-
+            
             // if password is same
             // generate a token with the user's id and the secretKey in the env file
-
             if (isSame) {
                 let token = jwt.sign({id: user.id}, process.env.secretKey, {
                     expiresIn: 1 * 24 * 60 * 60 * 1000,
@@ -70,7 +71,8 @@ const login = async (req, res) => {
                 res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
                 console.log("user", JSON.stringify(user, null, 2));
                 console.log(token);
-                //send user data
+                res.JSON({ token });
+                // send user data
                 return res.status(201).send(user);
             } else {
                 return res.status(401).send("Authentication failed");
