ac6bbf407ad3bb9304b44bfb6ff14c2a067929d6
melihcelik <melihsafacelik@Melihs-MacBook-Pro.local>
2022-09-05 15:20:08 +0300

    there is a bug occuring while connecting to the redis server

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const db = require('./models');
 const bodyParser = require("body-parser");
 const redis = require("redis");
 
-
 /* const express = require('express');
 const cookieParser = require('cookie-parser')
 const userRoutes = require('../routes/users') */
@@ -44,12 +43,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 const REDIS_PORT = process.env.REDIS_PORT;
 
 // default format of createClient() should have connect to localhost on port 6379 
+// const redisClient = redis.createClient();
 const redisClient = redis.createClient({
+  host: "127.0.0.1",
+  port: 6379,
   //url: 'redis://localhost:6379'
 });
 
+(async () => {
+  await redisClient.connect(); // connect is a async function.
+})();
+console.log(redisClient.isOpen);  // even is open returns true there is a error
+
 redisClient.on("error", (err) => {
     console.log("Could not establish a connection with redis!");
+    console.log(err);
 });
 redisClient.on("connect", (err) => {
     console.log("Connected to redis successfully!!!");
