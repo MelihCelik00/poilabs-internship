0f9e82df87834f2496e40d32ca6d694af02d52f9
MelihCelik00 <melihsafa.c@gmail.com>
2022-09-06 12:17:10 +0300

    env fix, started middleware auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,4 +3,8 @@ This middleware is used for authorization of the user.
 Controls the data from redis and due to token expiration time, authenticates the user.
 */
 
+const redisClient = require("../components/redis");
 
+const authUserFromRedis = async ( req, res ) => {
+    
+};
