6c8c4aa10970f6c47b76e7649cb075d69a2d15da
MelihCelik00 <melihsafa.c@gmail.com>
2022-09-06 15:01:46 +0300

    some configurational updates

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "development": {
-    "url":process.env.DEV_DATABASE_URL,
+    "url":"postgres://postgres:postgres@127.0.0.1:5432/poilabs",
     "dialect": "postgres",    
   },
   "test": {
