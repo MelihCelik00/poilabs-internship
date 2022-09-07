389a355d106e6057354d37fc19f40d190f690723
MelihCelik00 <melihsafa.c@gmail.com>
2022-09-07 16:04:19 +0300

    completed redis-token validation code

--- middlewares/tokenAuth.js
+++ middlewares/tokenAuth.js
@@ -4,36 +4,60 @@ Controls the data from redis and due to token expiration time, authenticates the
 */
 
 const redisClient = require("../components/redis");
-const config = require("../config/config")
+var createError = require('http-errors');
 const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
 
-const tokenAuth = (req, res, next) => {
-    /* We need req headers for a access to token */
-    console.log("x-access-token: ", req.headers["x-access-token"]);  // returns token
-    console.log("USER ID: ", req.headers.id); // returns user id
-
+const tokenAuth = async (req, res, next) => {
+    // req.headers["x-access-token"] // returns token
+    // req.headers.id) // returns user id
     let token = req.headers["x-access-token"];
-      /*
-       * TODO:
+      /** 
+        TODO:
             Implement redis tokens-user token comparison and validation
-            If user log in from different source, disable auth of older token
-       *
-       *   */ 
+            If user log in from different source, disable the auth of older token
+       **/ 
     if (token) {
-        jwt.verify(token, process.env.secretKey, function(err, decoded) {
-            if (err) {
-                console.log("ERR::: ", err);
-                return res.status(401).json({ "error": true, "message": 'Unauthorized access.', "err": err });
-            }else{
-                // Check redis
-                currentValidToken = redisClient.get(req.headers.id)
-                console.log("Current Valid Token: ", currentValidToken);
-                req.decoded = decoded;
-                next();
-                return res.status(200).next();
-            }
-        });
+        
+        const getIdFromToken = await redisClient.get(token);
+        console.log(getIdFromToken);
+        const getTokenFromId = await redisClient.get(getIdFromToken)
+        let parsedRedisTokenValue =  JSON.parse(getTokenFromId)
+        let tokenFromId = parsedRedisTokenValue["x-access-token"];
+        console.log(tokenFromId);
+        console.log("TOKENFROMID: ",tokenFromId);
+        if (token === tokenFromId) {
+            next();
+        } else {
+            await redisClient.del(token)
+            return res.status(401).json(createError.Unauthorized());
+        }
+
+
+
+        // const currentActiveTokenInfo = redisClient.get(req.headers.id).then(
+        //     reply => {
+        //         let redisJsonVal = JSON.parse(reply);
+        //         let redisToken = redisJsonVal["x-access-token"];
+        //         if (token == redisToken) {
+        //             console.log("Tokens are matching! ");
+        //             next();
+        //         } else {  // if jwt token is not stored in the redis db in related user id
+        //             redisClient.del(token).then(res.status(401).json(createError.Unauthorized()));
+        //         }
+        // });
+            // redisClient.del(redisJsonVal["x-access-token"]);
+        
+        
+        // if (token) {
+        // jwt.verify(token, process.env.secretKey, function(err, decoded) {
+        //     if (err) {  // mostly err.msg returns ExpiredTokenError in error-occured case.
+        //         return res.status(401).json(createError.Unauthorized());
+        //         //return res.status(401).json({ "error": true, "message": 'Unauthorized access.', "err": err });
+        //     }else{
+        //          req.decoded = decoded;
+        //          next();
+        //     }
+        // });
     } else {
         return res.status(403).send({
             "error" : true,
