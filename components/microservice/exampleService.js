94020095c041c7e8ca0f17b9fc24a89f572a546d
MelihCelik00 <melihsafa.c@gmail.com>
2022-09-07 12:42:38 +0300

    edited example microservice

--- components/microservice/exampleService.js
+++ components/microservice/exampleService.js
@@ -1,10 +1,10 @@
 const exService = (req, res, next) => {
     try {
-        res.status(200).send(`login successful with ${email} and ${password}`)
+        res.status(200).send(`login successful for user ${req.headers["id"]}!`)
         next();
     } catch (error) {
         console.log(error);
-        res.status(401).send("Not authorized to see this service!!!!!");
+        res.status(401).send(`Not authorized to see this service!!!!! Error: ${error}`);
     }
 }
 
