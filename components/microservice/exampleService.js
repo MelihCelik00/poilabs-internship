101299e583123808220d2835a6b674217fe0d90b
MelihCelik00 <melihsafa.c@gmail.com>
2022-09-12 09:40:31 +0300

    some swagger changes

--- components/microservice/exampleService.js
+++ components/microservice/exampleService.js
@@ -1,6 +1,7 @@
 const exService = (req, res, next) => {
     try {
-        res.status(200).send(`login successful for this user!`)
+        console.log(req.body);
+        res.status(200).json(`login successful for this user!`);
         next();
     } catch (error) {
         console.log(error);
