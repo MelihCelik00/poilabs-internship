9bfa9afd8d22279da4a93616c3293c6f5f507c62
MelihCelik00 <melihsafa.c@gmail.com>
2022-09-12 17:31:31 +0300

    IMPLEMENTED GET REQUEST IN SWAGGER

--- routes/testService.js
+++ routes/testService.js
@@ -3,6 +3,20 @@ const router = express.Router();
 const exampleService = require("../components/microservice/exampleService")
 const { exService } = exampleService
 
+
+// /**
+//  * @swagger
+//  * /api/services/orders:
+//  *      get:
+//  *          summary: Example get CRUD service
+//  *          tags: []
+//  *          responses:
+//  *              '200':
+//  *                  description: Successful operation
+//  *              500:
+//  *                  description: Some server error.
+//  */
+
 router.get('/orders/', exService);
 
 module.exports = router;
